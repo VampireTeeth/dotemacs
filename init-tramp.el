3eda3eff2d89312ee5c79b9d4c5ce37dabc209d8
Emagine <emagine@Emagines-MacBook-Pro-2.local>
2015-09-28 10:16:13 +1000

    multi-hop configuration for tramp

--- init-tramp.el
+++ init-tramp.el
@@ -1,3 +1,8 @@
 ;tramp mode setup
 (require 'tramp)
 (setq tramp-default-method "ssh")
+;tramp multihops that enables editing root files on remote host
+(add-to-list 'tramp-default-proxies-alist
+             '(nil "\\`root\\'" "/ssh:%h:"))
+(add-to-list 'tramp-default-proxies-alist
+             '((regexp-quote (system-name)) nil nil))
