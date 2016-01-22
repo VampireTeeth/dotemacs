81f0c8f698a4d3190763adff911ec97229d8edb0
Emagine <emagine@greglaptop.emagineinternational.com>
2016-01-22 12:42:49 +1100

    ace-window configuration tweaks

--- init-ace-window.el
+++ init-ace-window.el
@@ -1,4 +1,5 @@
 ;ace-window config
-;(global-set-key (kbd "M-p") 'ace-window)
-(global-set-key (kbd "C-x C-x") 'ace-window)
+(global-set-key (kbd "M-p") 'ace-window)
+;(global-set-key (kbd "C-x C-x") 'ace-window)
+(setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
 (global-set-key (kbd "RET") 'newline-and-indent)
