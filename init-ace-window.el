6105f3690734e1cdcd3994ff2e7e47c97c80e908
Emagine <emagine@greglaptop.emagineinternational.com>
2016-01-22 13:01:23 +1100

    ace-window conf changes

--- init-ace-window.el
+++ init-ace-window.el
@@ -1,5 +1,6 @@
 ;ace-window config
 (global-set-key (kbd "M-p") 'ace-window)
+(add-hook 'shell-mode-hook (lambda () (define-key shell-mode-map (kbd "C-c M-p") 'ace-window)))
 ;(global-set-key (kbd "C-x C-x") 'ace-window)
 (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
 (global-set-key (kbd "RET") 'newline-and-indent)
