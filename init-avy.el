756a6b8d87a3d706ced1227dd550511c08c685f8
steven <steven@VampireteethDebian>
2016-02-21 18:03:35 +1100

    key-binding for goto-char-1

--- init-avy.el
+++ init-avy.el
@@ -1,6 +1,6 @@
 ;Configuration for avy
 (avy-setup-default)
-(global-set-key (kbd "C-:") 'avy-goto-char)
+(global-set-key (kbd "C-\"") 'avy-goto-char)
 (global-set-key (kbd "C-'") 'avy-goto-char-2)
 (global-set-key (kbd "M-g f") 'avy-goto-line)
 (global-set-key (kbd "M-g w") 'avy-goto-word-1)
