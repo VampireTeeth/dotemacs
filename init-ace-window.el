c33f186937b925dd5f8238c7f021d4b35ecb7e78
steven <steven@VampireteethDebian>
2016-03-13 17:02:43 +1100

    changed ace-window keybinding

--- init-ace-window.el
+++ init-ace-window.el
@@ -1,6 +1,6 @@
 ;ace-window config
-(global-set-key (kbd "C-;") nil)
-(global-set-key (kbd "C-;") 'ace-window)
+(global-set-key (kbd "C-'") nil)
+(global-set-key (kbd "C-'") 'ace-window)
 ;(add-hook 'shell-mode-hook (lambda () (define-key shell-mode-map (kbd "C-c a w") 'ace-window)))
 ;(global-set-key (kbd "C-x C-x") 'ace-window)
 (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
